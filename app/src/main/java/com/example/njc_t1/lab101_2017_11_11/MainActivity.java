34c8f42143f474a166aaef622e6bb3f932ca9716
NJC-T1 <toda7.mk3@gmail.com>
2017-11-27 19:31:01 +0900

    2017-11-27の１回目コミット

--- app/src/main/java/com/example/njc_t1/lab101_2017_11_11/MainActivity.java
+++ app/src/main/java/com/example/njc_t1/lab101_2017_11_11/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     //グローバル変数の宣言
     MyCountDownTimer myCountDownTimer;
+    TextView textView2;
     Game game;
 
 
@@ -26,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView100 = (ImageView) findViewById(R.id.imageView100);
+        imageView100.setY(600);
 
         float x = 0;
         float y = 0;
@@ -40,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         TextView textView = (TextView)findViewById(R.id.textView);
 
-        TextView textView2 = (TextView)findViewById(R.id.textView2);
+        textView2 = (TextView)findViewById(R.id.textView2);
         textView2.setText("スタート位置は "+ xs + "と" + ys);
 
         // Ｙ座標を＋50する
@@ -49,6 +54,9 @@ public class MainActivity extends AppCompatActivity {
 
         //インスタンスの生成
         game = new Game();
+
+        ImageView imageview1 = (ImageView)findViewById(imageView1);
+        imageview1.setY(100);
 
         //タイマーのインスタンスの生成
         myCountDownTimer = new MyCountDownTimer(2*60*1000, 50);
@@ -76,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
         public void onTick(long millisUntilFinished){
             ImageView imageViewT = (ImageView)findViewById(imageView);
             float x = imageViewT.getX();
-            x = x+5;
+            x = x + 5;
             imageViewT.setX(x);
             TextView textViewT = (TextView)findViewById(textView);
             textViewT.setText("タイマが経過しました。");
@@ -91,6 +99,39 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        //参照値の取得
+        ImageView imageView100 = (ImageView) findViewById(R.id.imageView100);
+
+        int x = (int) event.getX();                    //タッチしたＸ座標
+        int y = (int) event.getY();                //タッチしたＹ座標
+
+        textView2.setText("X座標 "+x+"Y座標 " + y);
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                textView2.append("ACTION_DOWN");
+                //imageView100.setX(x);
+                break;
+            case MotionEvent.ACTION_UP:
+                textView2.append("ACTION_UP");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                textView2.append("ACTION_MOVE");
+                imageView100.setX(x);
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                textView2.append("ACTION_CANCEL");
+                break;
+        }
+
+        return true;
+
+    }
+
 
     public class  Enemy1 {
         int dir = +1;
@@ -118,6 +159,8 @@ public class MainActivity extends AppCompatActivity {
 
         //インスタンスの生成
         Enemy1 enemy1 = new Enemy1();
+
+
     }
 }
 
